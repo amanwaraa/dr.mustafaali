fbcf36881aa83427920567a53e88a34ce90ae00b
Maroshop <homworkhhh25@gmail.com>
2026-05-01 14:52:38 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyC9EMd_cnW3RofcYgfiZaVJZgMw5tHAFeE",
+    authDomain: "gvxzzz.firebaseapp.com",
+    databaseURL: "https://gvxzzz-default-rtdb.firebaseio.com",
+    projectId: "gvxzzz",
+    storageBucket: "gvxzzz.firebasestorage.app",
+    messagingSenderId: "481333488988",
+    appId: "1:481333488988:web:f5923a15a2406312012599",
+    measurementId: "G-2867JFZWCW"
+  },
+
+  firebaseRoot: "cashier_units_pro_gvxzzz_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_gvxzzz_885766842"
+  ]
+};
